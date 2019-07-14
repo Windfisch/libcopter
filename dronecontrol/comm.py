43aede59c7924afccde3abad311655835dd4a7c0
Florian Jung <flo@windfisch.org>
2019-07-14 19:32:37 +0200

    basic control demo

--- dronecontrol/comm.py
+++ dronecontrol/comm.py
@@ -2,6 +2,10 @@ import socket
 import command
 import time
 
+import cv2
+import libcopter
+from select import select
+
 #IP = "131.188.24.11"
 IP = "172.16.10.1"
 PORT = 8080
@@ -10,6 +14,7 @@ PORT = 8080
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # initialization sequence
+print("sending initialization sequence to the drone")
 sock.sendto(bytes([0x0f]), (IP, PORT))
 sock.sendto(bytes([0x28]), (IP, PORT)) # request UDP720P message
 print(sock.recvfrom(100))
@@ -28,6 +33,7 @@ sock.sendto(bytes([0x2c]), (IP, PORT)) # causes "ok\x02"
 #sock.sendto(bytes([0x27]), (IP, PORT))
 print(sock.recvfrom(100))
 
+print("initialization complete, now opening the video+telemetry TCP socket")
 
 # video and telemetry socket
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
@@ -38,14 +44,72 @@ for i in range(100): # receive some video
 	print(len(tcpsock.recv(1024)), end=' ')
 print()
 
+print("calibrating...")
+for i in range(10):
+	tcpsock.send(bytes([0,1,2,3,4,5,6,7,8,9, 0x28, 0x28]))
+	sock.sendto( command.make_command(0,0,0,0, recalibrate=True), (IP, PORT) )
+	time.sleep(0.1)
 
+p = libcopter.PyVideoTelemetryParser()
+height,yaw,pitch,roll = 0,0,0,0
+land,launch,panic = False,False,False
+i=0
+while True:
+	i+=1
+	print(i, "tcp recv")
+	while True:
+		readable, _, _ = select([tcpsock], [], [], 0)
+		if len(readable) == 0:
+			break
+		tcpdata = tcpsock.recv(1024)
+		print(len(tcpdata))
+		result = p.consume_data(tcpdata)
 
-#exit(1)
+		if len(result.video_frames) > 0:
+			cv2.imshow("frame", result.video_frames[-1])
+	
+	print(i, "tcp send")
+	tcpsock.send(bytes([0,1,2,3,4,5,6,7,8,9, 0x28, 0x28])) # this is required to start the video stream
+	
+	print(i, "waitkey")
+	k = cv2.waitKey(30)
+	if k == ord("w"):
+		pitch = min(pitch+0.25, 1)
+	elif k == ord("s"):
+		pitch = max(pitch-0.25, -1)
+	elif k == ord("d"):
+		roll = min(roll+0.25, 1)
+	elif k == ord("a"):
+		roll = max(roll-0.25, -1)
+	elif k == ord("e"):
+		yaw = min(yaw+0.25, 1)
+	elif k == ord("q"):
+		yaw = max(yaw-0.25, -1)
+	elif k == ord("r"):
+		height = min(height+0.25, 1)
+	elif k == ord("f"):
+		height = max(height-0.25, -1)
+	elif k == ord("x"):
+		launch = True
+	elif k == ord("z"):
+		land = True
+	elif k == ord("c"):
+		panic = True
+	elif k == ord("1") or k == ord("2") or k == ord("3"):
+		height,yaw,pitch,roll = 0,0,0,0
+	
+	print(i, "udp send")
+	
+	sock.sendto( command.make_command(height, yaw, pitch, roll, launch=launch, land=land, panic=panic), (IP, PORT) )
+
+	land,launch,panic = False,False,False
+		
+exit(1)
 print ("launching")
 print ("sending, in that order: \n%s\n%s\n%s\n%s" % (command.make_command(0,0,0,0, recalibrate=True).hex(), command.make_command(0,0,0,0, launch=True).hex(), command.make_command(0,0,0,0, land=True).hex(), command.make_command(0,0,0,0, panic=True).hex()))
 
 for i in range(10):
-	tcpsock.send(bytes([0,1,2,3,4,5,6,7,8,9, 0x25, 0x25]))
+	tcpsock.send(bytes([0,1,2,3,4,5,6,7,8,9, 0x28, 0x28]))
 	sock.sendto( command.make_command(0,0,0,0, recalibrate=True), (IP, PORT) )
 	time.sleep(0.1)
 	print("C ", end='')
