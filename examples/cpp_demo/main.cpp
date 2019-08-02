ac80487062946069d281121becfe0b577c6e3373
Florian Jung <florian.jung@fau.de>
2019-08-02 16:36:59 +0200

    extend the demo program

--- examples/cpp_demo/main.cpp
+++ examples/cpp_demo/main.cpp
@@ -7,10 +7,19 @@
 
 using namespace std;
 
+static float clamp(float val, float lo=-1.f, float hi=1.f)
+{
+	if (val < lo) return lo;
+	if (val > hi) return hi;
+	return val;
+}
+
 int main()
 {
 	SG500 drone;
 	drone.initialize();
+
+	float height=0, yaw=0, roll=0, pitch=0;
 
 	while (true)
 	{
@@ -18,10 +27,34 @@ int main()
 
 		cout << vid.size() << " / " << tel.size() << endl;
 		for (const auto& frame : vid)
-		{
 			cv::imshow("frame", frame.frame);
+
+		int key = cv::waitKey(30);
+
+		switch(key)
+		{
+			case 's': pitch += 0.25; break;
+			case 'w': pitch -= 0.25; break;
+			case 'd': roll  += 0.25; break;
+			case 'a': roll  -= 0.25; break;
+			case 'e': yaw   += 0.25; break;
+			case 'q': yaw   -= 0.25; break;
+			case 'r': height+= 0.25; break;
+			case 'f': height-= 0.25; break;
+			case 'x': drone.takeoff(); break;
+			case 'c': drone.panic(); break;
+			case 'z': drone.land(); break;
+			case '1':
+			case '2':
+			case '3': height=0; yaw=0; pitch=0; roll=0; break;
 		}
-		cv::waitKey(40);
+
+		roll = clamp(roll);
+		pitch = clamp(pitch);
+		yaw = clamp(yaw);
+		height = clamp(height);
+
+		drone.command(roll, pitch, yaw, height);
 	}
 	return 0;
 }
